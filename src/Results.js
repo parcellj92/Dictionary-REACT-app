f6dec3392df4c48e23ce3718210256eba01a885a
parcellj92 <104948868+parcellj92@users.noreply.github.com>
2022-11-11 15:22:10 -0700

    added in the phonetics component, and mapping for audio and text

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning.js";
+import Phonetic from "./Phonetic.js";
 import "./Meaning.css";
 
 export default function Results(props) {
@@ -7,7 +8,13 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h1>{props.results.word}</h1>
-
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
